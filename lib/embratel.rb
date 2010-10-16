d1e2981ce9dfb9ffcf0059cef567419b6e8f73cb
Murilo Soares Pereira <murilo.soares3@gmail.com>
2010-10-16 18:59:41 -0300

    Don't rely on bundler to require stuff.

--- lib/embratel.rb
+++ lib/embratel.rb
@@ -1,17 +1,3 @@
-require 'rubygems'
-
-begin
-  require 'bundler'
-rescue LoadError
-  STDERR.puts "You need to install bundler"
-else
-  begin
-    Bundler.setup
-  rescue Bundler::BundlerError
-    STDERR.puts $!
-  end
-end
-
 require 'embratel/phone_bill'
 require 'embratel/call'
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,8 @@
-require 'test/unit'
 
 $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+require 'rubygems'
 require 'embratel'
+require 'test/unit'
 
 class Test::Unit::TestCase
 end
