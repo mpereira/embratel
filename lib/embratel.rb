2d149ed1612f98c7bf96fd2a891fa942b62d8291
Murilo Pereira <murilo@murilopereira.com>
2011-03-20 18:19:18 -0300

    1.0.

--- lib/embratel.rb
+++ lib/embratel.rb
@@ -9,7 +9,7 @@ require 'embratel/call.rb'
 require 'embratel/phone_bill.rb'
 
 module Embratel
-  VERSION = '0.0.3'
+  VERSION = '1.0.0'
 
   class InvalidPhoneBillFileError < StandardError; end
 end
